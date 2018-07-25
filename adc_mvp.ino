0b13f32ce9b62723d4d73ef4e830d89072d5b479
Zenith <zenith.eesc@gmail.com>
2018-07-25 01:23:43 -0300

    Update adc_mvp.ino

--- adc_mvp.ino
+++ adc_mvp.ino
@@ -13,7 +13,7 @@ int acel_time = 0;
 int desacel_time = 0;
 int Pin = 9;
 bool Dir = 1;
-int Dir_Pin = 7;
+int Dir_Pin = 8;
 int set_delay = 5;
 long start_time_ms = 5000;
 int start_vel = 50;
@@ -24,6 +24,9 @@ int desacel_torque = -10;
 int e = 0;
 int encoder_speed = 0;
 int multiplier = 1;
+String direction_rw = "";
+bool run_rw = false;
+float boost = 1;
 
 double Setpoint, Input, Output;
 PID myPID(&Input, &Output, &Setpoint,0.2,1,0.1, DIRECT);
@@ -51,33 +54,39 @@ void setup(void)
   bno.setExtCrystalUse(true);
   pinMode(Pin, OUTPUT);
   pinMode(Dir_Pin, OUTPUT);
-  start();
   imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   w = gyro.z();
   Input = w;
   Setpoint = 0;
-  myPID.SetOutputLimits(-100, 150);
+  myPID.SetOutputLimits(-100, 100);
   myPID.SetMode(AUTOMATIC);
+  analogWrite(Pin, 0);
 }
 
 void loop(void)
 {
   request_action();
+  delay(300);
+  Serial.println("on loop");
+  start();
 }
 void start(){
-  delay(1000);
-  set_dir();
-  stabilize();
+  if ( abs(get_gyro_z()) > 200 && run_rw){
+    set_dir();
+    stabilize();
+  }
 }
 
 void set_dir(){
   float set_dir_speed = get_gyro_z();
   if (set_dir_speed > 0){
     digitalWrite(Dir_Pin, 1);
-    multiplier = 1;
+    multiplier = -1;
+    direction_rw = "cw";
   }else{
     digitalWrite(Dir_Pin, 0);
-    multiplier = -1;
+    multiplier = 1;
+    direction_rw = "ccw";
   }
 }
 
@@ -89,24 +98,34 @@ float get_gyro_z(){
 
 void stabilize(){
   while(1){
-  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  delay(300);
-  w = gyro.z()*multiplier;
-  Input = w;
-  myPID.Compute();
-  vel = 100 + Output;
-  Serial.println(vel);
-  analogWrite(Pin, vel);
+    Serial.print("on stabilize, direction");Serial.println(direction_rw);
+    imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+    delay(300);
+    w = gyro.z()*multiplier;
+    Serial.println(w);
+    
+    if (abs(w) > 200){
+      boost = 1.5;
+    }else{
+      boost = 1;
+    }
+    Input = w;
+    
+    myPID.Compute();
+    vel = 100 + Output*boost;
+    Serial.println(vel);
+    analogWrite(Pin, vel);
   }
 }
 
 void request_action(){
+  char c = 'o';
   Wire.requestFrom(9, 1); 
   while (Wire.available()) {  
-    char c = Wire.read();
+    c = Wire.read();
   }
   if (c == 'r'){
-    start();
+    run_rw = true;
   }
 }
     
